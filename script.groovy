8be51845a02fa127e412dfef273e93101679b38e
hanah.benghz <hanah.benghz@gmail.com>
2022-12-07 19:56:48 +0100

    fix: docker pass via stdin

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t hanaghz/demo-app:${IMAGE_NAME} .'
-        sh "echo $PASS | docker login -u $USER -p"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push hanaghz/demo-app:${IMAGE_NAME}'
     }
 }
