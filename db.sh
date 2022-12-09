7549f89b2838ad95b1a2363c6ef229b1c81be110
zormati <mohamedalizormati@insat.u-carthage.tn>
2022-12-09 09:36:46 +0100

    modified IP addr

--- db.sh
+++ db.sh
@@ -1,3 +1,1 @@
-docker run -d -e MYSQL_ROOT_PASSWORD=hanah --name school-library-db  \
--e MYSQL_DATABASE=school_library -p 3307:3306 \
--v /var/lib/docker/volumes/project-devops-volume/_data:/var/lib/mysql mysql:latest
+docker run -d -e MYSQL_ROOT_PASSWORD=hanah --name school-library-db  -e MYSQL_DATABASE=school_library -p 3307:3306 -v /var/lib/docker/volumes/project-devops-volume/_data:/var/lib/mysql mysql:latest
